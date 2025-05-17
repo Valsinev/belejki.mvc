01604b0e4d3baa698c180f20e217d48f4b686e01
Valsinev <streamline_@abv.bg>
2025-05-17 21:08:15 +0300

    Fixed locale settings

--- src/main/java/belejki/com/mvc/config/Configuration.java
+++ src/main/java/belejki/com/mvc/config/Configuration.java
@@ -6,6 +6,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -23,13 +24,23 @@ public class Configuration implements WebMvcConfigurer {
         return messageSource;
     }
 
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        SessionLocaleResolver slr = new SessionLocaleResolver();
+//        slr.setDefaultLocale(Locale.ENGLISH);
+////        slr.setDefaultLocale(Locale.forLanguageTag("bg"));
+//        return slr;
+//    }
+
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.ENGLISH);
-//        slr.setDefaultLocale(Locale.forLanguageTag("bg"));
-        return slr;
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.ENGLISH);
+        clr.setCookieName("lang");
+        clr.setCookieMaxAge(3600); // 1 hour
+        return clr;
     }
+
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
